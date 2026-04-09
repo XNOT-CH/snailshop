6f81827f1ee35899d55bfdce9575cc25c59524d7
XNOT CH <xnot.ch110249@example.com>
2026-04-09 14:14:50 +0700

    Add next-auth session typings

---
+++ next-auth.d.ts
@@ -0,0 +1,25 @@
+import type { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+    interface Session {
+        user: DefaultSession["user"] & {
+            id: string;
+            role?: string;
+            username?: string;
+        };
+    }
+
+    interface User {
+        id: string;
+        role?: string;
+        username?: string;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id?: string;
+        role?: string;
+        username?: string;
+    }
+}
